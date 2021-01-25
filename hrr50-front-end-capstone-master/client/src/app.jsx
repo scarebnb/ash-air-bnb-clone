8a3f877fa46096c8cc3862f25407ea7917a24ebf
Brian Smit <brianjsmit@gmail.com>
2021-01-25 15:08:23 -0800

    Fixed the routes one last time

--- hrr50-front-end-capstone-master/client/src/app.jsx
+++ hrr50-front-end-capstone-master/client/src/app.jsx
@@ -29,7 +29,7 @@ class Gallery extends React.Component {
   componentDidMount() {
     axios({
       method: 'get',
-      url: '/photos',
+      url: 'http://localhost:3000/photos',
     }).then((data) => {
       const photoArray = [];
       const property = {
